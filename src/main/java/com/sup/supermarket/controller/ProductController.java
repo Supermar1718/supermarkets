d8af6da198694d8ea64623325da9f8ac8850b2dd
backend <supermarket201917@gmail.com>
2024-10-12 17:03:00 +0530

    Added crud operation for product

--- src/main/java/com/sup/supermarket/controller/ProductController.java
+++ src/main/java/com/sup/supermarket/controller/ProductController.java
@@ -1,13 +1,51 @@
 package com.sup.supermarket.controller;
 
+import com.sup.supermarket.entity.Product;
 import com.sup.supermarket.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
 public class ProductController {
     @Autowired
     private ProductService service;
+
+    //create the product
+    @PostMapping("/save")
+    public ResponseEntity<Product> registerProduct(@RequestBody Product product) {
+        Product saveProduct = service.saveProduct(product);
+        return ResponseEntity.ok(saveProduct);
+    }
+
+    //get List of Product
+    @GetMapping("/list")
+    private ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> allProduct = service.getAllProduct();
+        return ResponseEntity.ok(allProduct);
+    }
+
+    //get Product by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable String id) {
+        Product productById = service.getProductById(id);
+        return ResponseEntity.ok(productById);
+    }
+
+    //update Product
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product, String id) {
+        Product updateProduct = service.updateProduct(product, id);
+        return ResponseEntity.ok(updateProduct);
+    }
+
+    //delete product by Id
+    private ResponseEntity<String> deleteProductById(@PathVariable String id) {
+        service.deleteProduct(id);
+        return ResponseEntity.ok("Product Deleted Successfully" + id);
+    }
+
 }
--- src/main/java/com/sup/supermarket/service/ProductService.java
+++ src/main/java/com/sup/supermarket/service/ProductService.java
@@ -1,11 +1,48 @@
 package com.sup.supermarket.service;
 
+import com.sup.supermarket.entity.Product;
+import com.sup.supermarket.exception.ProductNotFoundException;
 import com.sup.supermarket.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository repository;
+
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public List<Product> getAllProduct() {
+        return repository.findAll();
+    }
+
+    public Product getProductById(String id){
+        Product product = repository.findById(id).orElseThrow(() -> new ProductNotFoundException("Product is not found with Id: " + id));
+        return product;
+    }
+
+    public Product updateProduct(Product product, String id){
+        Product existingProduct = repository.findById(id).orElseThrow(()->new ProductNotFoundException("Product does not exist with id:"+id));
+        existingProduct.setName(product.getName());
+        existingProduct.setDescription(product.getDescription());
+        existingProduct.setBarcode(product.getBarcode());
+        existingProduct.setBrand(product.getBrand());
+        existingProduct.setExpirationDate(product.getExpirationDate());
+        existingProduct.setImageUrl(product.getImageUrl());
+        existingProduct.setCategory(product.getCategory());
+        existingProduct.setStockQuantity(product.getStockQuantity());
+        existingProduct.setSupplierId(product.getSupplierId());
+        existingProduct.setUnit(product.getUnit());
+        existingProduct.setWeight(product.getWeight());
+        return repository.save(existingProduct);
+    }
+
+    public void deleteProduct(String id){
+        repository.deleteById(id);
+    }
 }
